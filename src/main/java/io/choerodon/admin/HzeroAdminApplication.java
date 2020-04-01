879728d29742c619aacd92205e040caa2dca2625
scp <changping.shi@hand-china.com>
2020-04-01 14:21:04 +0800

    [ADD] 初始化服务

---
+++ src/main/java/io/choerodon/admin/HzeroAdminApplication.java
@@ -0,0 +1,13 @@
+package io.choerodon.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzeroAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzeroAdminApplication.class, args);
+    }
+
+}
