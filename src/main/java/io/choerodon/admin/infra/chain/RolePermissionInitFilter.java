9a5b417aad809e026ae1559798d0e11b75b61b71
王灏 <2507172753@qq.com>
2020-08-10 15:27:11 +0800

    [imp]调整权限刷新代码
    (cherry picked from commit 9ed7eea63bbe31552581ff4e0284bc5178b800e6)

--- src/main/java/io/choerodon/admin/infra/chain/RolePermissionInitFilter.java
+++ src/main/java/io/choerodon/admin/infra/chain/RolePermissionInitFilter.java
@@ -1,12 +1,16 @@
 package io.choerodon.admin.infra.chain;
 
+import org.apache.commons.lang3.StringUtils;
 import org.hzero.admin.domain.vo.InitChainContext;
+import org.hzero.admin.domain.vo.Service;
 import org.hzero.admin.infra.chain.InitChain;
 import org.hzero.admin.infra.chain.InitFilter;
+import org.hzero.core.redis.RedisHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
@@ -21,19 +25,30 @@ import io.choerodon.admin.infra.feign.IamClient;
 @Component
 public class RolePermissionInitFilter implements InitFilter, Ordered {
 
+    private static final String CHOERODON_ROLE_PERMISION_ASYNC_PREFIX = "choerodon:permission:async";
+
     @Autowired
     private IamClient iamClient;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RolePermissionInitFilter.class);
 
 
     @Override
     public void doFilter(InitChain chain, InitChainContext context) {
-        LOGGER.info(">>>>>>>>> start async role permission >>>>>>>>>>>>>>");
-        ResponseEntity<Void> response = iamClient.asyncRolePermision();
-
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            LOGGER.error("refresh role permission failed, cause: " + (response.getBody() == null ? response.getStatusCodeValue() : response.getBody()));
+        LOGGER.info(">>>>>>>>> do async role permission filter<<<<<<<<<<<<");
+        Service service = context.getService();
+        String key = CHOERODON_ROLE_PERMISION_ASYNC_PREFIX + ":" + service.getServiceName() + ":" + service.getVersion();
+        boolean notAsyncFlag = StringUtils.isEmpty(stringRedisTemplate.opsForValue().get(key));
+        if (notAsyncFlag) {
+            LOGGER.info(">>>>>>>>> start async role permission. serviceName:{}, serviceVersion:{} >>>>>>>>>>>>>>", service.getServiceName(), service.getVersion());
+            ResponseEntity<Void> response = iamClient.asyncRolePermision();
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                LOGGER.error("refresh role permission failed, cause: " + (response.getBody() == null ? response.getStatusCodeValue() : response.getBody()));
+            } else {
+                stringRedisTemplate.opsForValue().set(key, "true");
+            }
         }
         chain.initNext(chain, context);
     }
