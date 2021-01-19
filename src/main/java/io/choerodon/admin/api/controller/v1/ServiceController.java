5aa0ee130a6dbd615fd90a55c01b541117f47211
18986 <changping.shi@hand-chian.com>
2021-01-19 15:00:41 +0800

    [ADD] 添加部署模块查询接口

--- src/main/java/io/choerodon/admin/api/controller/v1/ServiceController.java
+++ src/main/java/io/choerodon/admin/api/controller/v1/ServiceController.java
@@ -1,16 +1,19 @@
 package io.choerodon.admin.api.controller.v1;
 
 import java.util.List;
+import java.util.Set;
 
 import io.swagger.annotations.ApiOperation;
 import org.hzero.admin.app.service.HServiceService;
 import org.hzero.admin.domain.entity.HService;
+import org.hzero.core.util.Results;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.choerodon.admin.app.service.ServiceC7nService;
 import io.choerodon.core.iam.ResourceLevel;
 import io.choerodon.swagger.annotation.Permission;
 
@@ -27,11 +30,21 @@ public class ServiceController {
 
     @Autowired
     private HServiceService serviceService;
+    @Autowired
+    private ServiceC7nService serviceC7nService;
+
 
     @GetMapping
     @Permission(level = ResourceLevel.ORGANIZATION, permissionLogin = true)
     @ApiOperation("查询已经部署的应用服务")
     public ResponseEntity<List<HService>> listServices() {
         return ResponseEntity.ok(serviceService.selectServices(null));
+    }
+
+    @GetMapping(value = "/model")
+    @Permission(permissionPublic = true)
+    @ApiOperation(value = "获取已经部署的所有模块")
+    public ResponseEntity<Set<String>> listModels() {
+        return Results.success(serviceC7nService.listModels());
     }
 }
